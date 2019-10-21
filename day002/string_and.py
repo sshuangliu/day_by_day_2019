e6d445c90790de5345beb169372884fc13c97502
shuang <shunagliu007@gmail.com>
2019-10-21 22:58:48 +0800

    all 005

--- day002/string_and.py
+++ day002/string_and.py
@@ -38,5 +38,40 @@ depart2_m = 'shuang_liu'
 COURSE_FEES_SEC = 456789.123456
 COURSE_FEES_Python = 1234.3456
 
+line1 = ('Department1 name:%s  Manager:%s       COURSE FEES:%.2f The End!'%(department1,depart1_m,COURSE_FEES_SEC))
+line2 = ('Department2 name:%s    Manager:%s    COURSE FEES:%.2f   The End!'%(department2,depart2_m,COURSE_FEES_Python))
+# infor = {
+# 'department1' : 'Security',
+# 'department2' : 'Python',
+# 'depart1_m' : 'cq_bomb',
+# 'depart2_m' : 'shuang_liu',
+# 'COURSE_FEES_SEC' : 456789.123456,
+# 'COURSE_FEES_Python' : 1234.3456,
+# }
+#line1 = ('Department1 name:{department1}  Manager:{depart1_m}       COURSE FEES:{COURSE_FEES_SEC:.2f} The End!'.format(**infor))
+#line2 = ('Department2 name:{department2}    Manager:{depart2_m}    COURSE FEES:{COURSE_FEES_Python:.2f}   The End!'.format(**infor))
 
+length = len(line1)
+print('='*length)
+print(line1)
+print(line2)
+print('='*length)
+
+
+
+##5:完成课堂作业(2)
+#RE匹配IP地址，并格式化打印结果
+
+str1 = 'Port-channel1.189    192.168.189.254    YES    CONFIG    UP'
+str2 = str1.split(' ')
+print(str2)
+print()
+
+while True:
+    if str2.count(''):
+        str2.remove('')
+    else:
+        break
+print(str2)
+print('接口\t\t:{0[0]}\nip地址\t:{0[1]}\n状态\t\t:{0[2]}'.format(str2))
 
