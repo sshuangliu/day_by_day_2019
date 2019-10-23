12362b77d3f7e4e2c32eff69c39e6b2865558af5
sshuangliu <38092313+sshuangliu@users.noreply.github.com>
2019-10-23 22:03:45 +0800

    Update day004.py
    add

--- day004.py
+++ day004.py
@@ -7,9 +7,22 @@ ens160 = '''flags=4163<UP,BROADCAST,RUNNING,MULTICAST> mtu 1500
               RX packets 174598769 bytes 1795658527217 (1.6 TiB)
               RX errors 1 dropped 24662 overruns 0 frame 0
               TX packets 51706604 bytes 41788673420 (38.9 GiB)
-              TX errors 0 dropped 0 overruns 0 carrier 0 collisions 0
-              '''
-pattern1 = re.compile(r'inet\s+(25[0-5]\.|2[0-4]\d\.|[01]?\d\d?\.{3}25[0-5]|2[0-4]\d|[01]?\d\d?)')
-ipv4add = pattern1.findall(ens160)
+              TX errors 0 dropped 0 overruns 0 carrier 0 collisions 0   
+                  '''
+# 字符串重复---小括号
+# ?: 非捕获分组
+# ?<= 非捕获 匹配后面表达式 且该字符串前面字符匹配前面匹配表达式
+pattern1 = re.compile(r'(?<=inet\s)\b(?:25[0-5]\.|2[0-4]\d\.|[01]?\d\d?\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\b')
+pattern2 = re.compile(r'(?<=netmask\s)\b(?:25[0-5]\.|2[0-4]\d\.|[01]?\d\d?\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\b')
+pattern3 = re.compile(r'(?<=broadcast\s)\b(?:25[0-5]\.|2[0-4]\d\.|[01]?\d\d?\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\b')
+pattern4 = re.compile(r'(?<=ether\s)\b(?:[0-9a-f]{2}:){5}(?:[0-9a-f]{2})\b')
+
+ipv4add = pattern1.findall(ens160)[0]
+netmask = pattern2.findall(ens160)[0]
+broadcast = pattern3.findall(ens160)[0]
+macadd = pattern4.findall(ens160)[0]
+
 print(ipv4add)
-# https://blog.csdn.net/sxingming/article/details/52244728
+print(netmask)
+print(broadcast)
+print(macadd)
