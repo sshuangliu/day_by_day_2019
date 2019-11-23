45fa17041402af4974c4815a9e4ffc46bb80905b
shuang <shunagliu007@gmail.com>
2019-11-23 13:35:35 +0800

    matplotlib

--- day104/matplotlib_pie chart.py
+++ day104/matplotlib_pie chart.py
@@ -14,8 +14,31 @@ plt.rcParams['font.family'] = 'sans-serif'
 
 
 def pie_chart(size_list,name_list):
-    pass
+    #
+    plt.figure(figsize=(6,6))
 
+
+    explode = (0.01,0.01,0.01,0.01)
+
+    patches , lable_text, percent_text = plt.pie(size_list,
+                                                 explode=explode,
+                                                 labels=name_list,
+                                                 labeldistance=1.1,
+                                                 autopct='%3.1f%%',
+                                                 shadow=False,
+                                                 startangle=90,
+                                                 pctdistance=0.6
+                                                 )
+    for l in lable_text:
+        l.set_size = 30
+    for p in percent_text:
+        p.set_size = 20
+
+    plt.axis('equal')
+    plt.legend()
+    plt.show()
 
 if __name__ == '__main__':
-    pass
+    counters = [30,53,12,45]
+    protocols = ['http','rdp','ftp','other']
+    pie_chart(counters,protocols)
