aea8026d67d4bb06dd181e597d79fc5f1b3a1482
shuang <shunagliu007@gmail.com>
2019-11-23 19:14:45 +0800

    matplotlib

--- day104/matplotlib_pie chart.py
+++ day104/matplotlib_pie chart.py
@@ -14,31 +14,49 @@ plt.rcParams['font.family'] = 'sans-serif'
 
 
 def pie_chart(size_list,name_list):
-    #
+    # 设置图片大小:长宽
     plt.figure(figsize=(6,6))
 
-
+    # 爆炸出来每一部分，以及每部分之间的间隙
     explode = (0.01,0.01,0.01,0.01)
 
-    patches , lable_text, percent_text = plt.pie(size_list,
+    # pie 饼状图的函数，有返回值. 传入参数：
+    # size_list 各个部分的数值大小，会自动计算他们之间的相对百分比
+    # explode 是否爆炸，可以不传入，则为None
+    # labels 每部分的标签文字
+    # labeldistance 标签文字距离圆心的半径倍数
+    # autopct 扇形区域数值格式 3位整数，2位小数的百分数
+    # shadow 是否有阴影
+    # startangle 画图的起始角度，90度起始 逆时针绘制图形
+    # pctdistance 百分比数值 距离圆心的半径倍数
+    patches, lable_text, percent_text = plt.pie(size_list,
                                                  explode=explode,
                                                  labels=name_list,
                                                  labeldistance=1.1,
-                                                 autopct='%3.1f%%',
+                                                 autopct='%3.2f%%',
                                                  shadow=False,
                                                  startangle=90,
                                                  pctdistance=0.6
                                                  )
+    print(lable_text)
+    print(percent_text)
+    print(patches)
+    # pie函数返回值 lable_text,percent_text ,如果需要改变文本大小，可以for遍历
+    # 然后set_size设置其大小
     for l in lable_text:
         l.set_size = 30
     for p in percent_text:
         p.set_size = 20
 
+    # 横纵坐标 比例相等，保证图形是圆的
+    # plt.axis([-1, 10, 0, 6]) 横轴-1 10.纵轴0 6
     plt.axis('equal')
     plt.legend()
     plt.show()
 
 if __name__ == '__main__':
+
     counters = [30,53,12,45]
     protocols = ['http','rdp','ftp','other']
-    pie_chart(counters,protocols)
+    pie_chart(counters, protocols)
+
--- temp_test_001.py
+++ temp_test_001.py
@@ -65,5 +65,17 @@ import logging
 #     print(e)
 
 
-import matplotlib
-print(matplotlib.matplotlib_fname())
+import matplotlib.pyplot as plt
+import numpy as np
+
+def f(t):
+    return np.exp(-t)*np.cos(2*np.pi*t)
+a = np.arange(0.0,5.0,0.02)
+
+plt.subplot(2,1,1)
+plt.plot(a,f(a))
+
+plt.subplot(211)
+plt.plot(a,np.cos(2*np.pi*a),'r--')
+
+plt.show()
